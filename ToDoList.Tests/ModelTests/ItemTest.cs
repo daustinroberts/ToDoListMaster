2bc913d5c6b849e413bf3d200ad1df589c6243f7
Guest User <Guest@epicodus-70.local>
2018-05-03 10:04:21 -0700

    This adds save assigned id to object and Find it in Database tests

--- ToDoList.Tests/ModelTests/ItemTest.cs
+++ ToDoList.Tests/ModelTests/ItemTest.cs
@@ -13,56 +13,74 @@ namespace ToDoListApp.Tests
     {
         public void Dispose()
         {
-            // Item.DeleteAll();
+            Item.DeleteAll();
         }
         public ItemTests()
         {
             DBConfiguration.ConnectionString = "server=localhost;user id=root;password=root;port=8889;database=todo_test;";
         }
-        [TestMethod]
-        public void GetAll_DbStartsEmpty_0()
-        {
-          //Arrange
-          //Act
-          int result = Item.GetAll().Count;
+        // [TestMethod]
+        // public void GetAll_DbStartsEmpty_0()
+        // {
+        //   //Arrange
+        //   //Act
+        //   int result = Item.GetAll().Count;
+        //
+        //   //Assert
+        //   Assert.AreEqual(0, result);
+        // }
+        // [TestMethod]
+        // public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+        // {
+        //   // Arrange, Act
+        //   Item firstItem = new Item("Mow the lawn");
+        //   Item secondItem = new Item("Mow the lawn");
+        //
+        //   // Assert
+        //   Assert.AreEqual(firstItem, secondItem);
+        // }
+        // [TestMethod]
+        // public void Save_SavesToDatabase_ItemList()
+        // {
+        //   //Arrange
+        //   Item testItem = new Item("Mow the lawn");
+        //
+        //   //Act
+        //   testItem.Save();
+        //   List<Item> result = Item.GetAll();
+        //   Console.WriteLine(result.Count);
+        //   foreach(Item element in result)
+        //   {
+        //     Console.WriteLine("result: " + result);
+        //   }
+        //
+        //
+        //   List<Item> testList = new List<Item>{testItem};
+        //   Console.WriteLine(testList.Count);
+        //   Console.WriteLine("testItem "+testItem);
+        //   Console.WriteLine("testList " +testList);
+        //   //Assert
+        //   CollectionAssert.AreEqual(testList, result);
+        // }
 
-          //Assert
-          Assert.AreEqual(0, result);
-        }
         [TestMethod]
-        public void Equals_ReturnsTrueIfDescriptionsAreTheSame_Item()
+        public void Save_AssignsIdToObject_Id()
         {
-          // Arrange, Act
-          Item firstItem = new Item("Mow the lawn");
-          Item secondItem = new Item("Mow the lawn");
-
-          // Assert
-          Assert.AreEqual(firstItem, secondItem);
-        }
-        [TestMethod]
-        public void Save_SavesToDatabase_ItemList()
-        {
-          //Arrange
+          //Arranges
           Item testItem = new Item("Mow the lawn");
 
           //Act
           testItem.Save();
-          List<Item> result = Item.GetAll();
-          Console.WriteLine(result.Count);
-          foreach(Item element in result)
-          {
-            Console.WriteLine("result: " + result);
-          }
+          Item savedItem = Item.GetAll()[0];
 
+          int result = savedItem.GetId();
+          int testId = testItem.GetId();
 
-          List<Item> testList = new List<Item>{testItem};
-          Console.WriteLine(testList.Count);
-          Console.WriteLine("testItem "+testItem);
-          Console.WriteLine("testList " +testList);
+          Console.WriteLine("testId: " + testId);
+          Console.WriteLine("result: " + result);
           //Assert
-          CollectionAssert.AreEqual(testList, result);
+          Assert.AreEqual(testId, result);
         }
-
         [TestMethod]
         public void Find_FindsItemInDatabase_Item()
         {
@@ -76,5 +94,6 @@ namespace ToDoListApp.Tests
           //Assert
           Assert.AreEqual(testItem, foundItem);
         }
+
     }
 }
